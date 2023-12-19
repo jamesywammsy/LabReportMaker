2506965b72666bad5c600bb67477b52c1681d9d1
jamesywammsy <142356528+jamesywammsy@users.noreply.github.com>
2023-12-19 10:07:31 -0500

    Add files via upload

---
+++ PerfectIntegers.java
@@ -0,0 +1,38 @@
+package testing;
+
+public class PerfectIntegers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int integer = 0;
+		int sum = 0;
+
+		for (int a = 1; a <= 10000; a++) {
+			
+			sum = 0;
+			integer = a;
+			
+			for (int i = 1; i < a-1; i++) {
+				
+				if (a%i == 0) {
+					
+					sum += i;
+					
+				}
+				
+				
+				
+			}
+			
+			if (sum == integer) {
+				
+				System.out.println( "perfect integer is "+integer);
+				
+			}
+
+		}
+
+	}
+
+}
